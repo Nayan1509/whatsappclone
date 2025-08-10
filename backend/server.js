b7f4f69236ccb2d13daa8374cb013d5e5ea24e91
Anunay Nayan <anunayan123@gmail.com>
2025-08-10 19:46:35 +0530

    CORS error handled

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,27 @@ const connectDB = require("./config/db");
 dotenv.config();
 connectDB();
 
+const app = express();
+
+// CORS setup
+const allowedOrigins = [
+  process.env.CLIENT_ORIGIN,
+  "http://localhost:5173", 
+];
+
 const corsOptions = {
-  origin: process.env.CLIENT_ORIGIN, // "https://whatsappclone-sooty.vercel.app"
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
 
-const app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
 
@@ -21,6 +35,3 @@ app.use("/", require("./routes/MessageRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-app.get("/", (req, res) => {
-  res.send("Backend is running...");
-});
